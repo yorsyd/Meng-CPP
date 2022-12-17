7bcda489a49f281bc1d8127bbfe399c8991727a8
yorsyd <91246717+yorsyd@users.noreply.github.com>
2022-12-18 04:46:41 +0700

    Delete nyoba.cpp

--- Ngajar/Array/nyoba.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-void nilai(float a=2.5,float b=3.5){
-	float hasil=a+b;
-	cout<<"Penjumlahan nilai "<<a<<" dan nilai "<<b<<" adalah : "<<hasil<<endl;
-}int main(){
-	nilai();
-}
